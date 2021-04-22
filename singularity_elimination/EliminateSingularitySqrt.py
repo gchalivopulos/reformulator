84d2fce3dbba94ec457020468e0591beb7d05824
nikoskazazakis <nikos.kazazakis@octeract.co.uk>
2021-04-22 16:18:55 +0100

    Added mod for sqrt(f)

---
+++ singularity_elimination/EliminateSingularitySqrt.py
@@ -0,0 +1,16 @@
+from octeract import *
+
+# Eliminate domain-based singularities for all square root terms
+# If the function inside a square root term can be negative,
+# add a constraint to ensure that it's always positive.
+
+# Symbolic match for any term of type constant*log(f)
+my_match = Match("C(n)*sqrt(E(f))")
+
+# Only select the ones that might cross into negative domains
+my_filter = IsMaybeFeasible("f <= -1.e-12")
+
+# Add a constraint to enforce positivity
+my_actions = AddConstraint("f >= 1.e-6")
+
+EliminateSingularitySqrtMod = (my_match & my_filter).then(my_actions)
--- singularity_elimination/eliminate_singularities.py
+++ singularity_elimination/eliminate_singularities.py
@@ -1,15 +1,19 @@
 from octeract import *
 from EliminateSingularityLog import EliminateSingularityLogMod
+from EliminateSingularitySqrt import EliminateSingularitySqrtMod
+
 
 m = Model()
 
 m.add_variable("y",0,10)
-m.minimize("log(x^3+x^2+2)")
+m.minimize("log(x^3+x^2+2)+sqrt(x^3+2*x+2)")
 
 m.set("x").to("log(y+1)")
+m.set("x").to("sqrt(y+1)")
 
 print(m)
 
 m.apply_mod(EliminateSingularityLogMod)
+m.apply_mod(EliminateSingularitySqrtMod)
 
 print(m)
