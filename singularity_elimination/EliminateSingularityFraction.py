ac4470bf476e99ea071bc01928eeb2d5b4a37bde
nikoskazazakis <nikos.kazazakis@octeract.co.uk>
2021-04-29 16:29:51 +0100

    added mod to eliminate singularities

---
+++ singularity_elimination/EliminateSingularityFraction.py
@@ -0,0 +1,25 @@
+from octeract import *
+
+# Symbolically match any fraction
+my_match = Match("C(n)*E(f1)/E(f2)")
+
+# Grab only the ones that could have zero denominators
+my_filter = IsMaybeFeasible("f2==0")
+
+# Add auxiliary variable with the range of f1/f2 as bounds
+add_auxiliary_var = AddVariableSpan('w','f1/f2')
+
+# Substitute f1/f2 with new var w
+sub = SubWith("w")
+
+# Remove the fraction by multiplying with f2
+add_constraint1 = AddConstraint("fraction", "f2*w == f1")
+
+# Enforce f2 != 0 for consistency
+add_constraint2 = AddConstraint("denominator", "abs(f2) > 1.e-6")
+
+# Chain actions
+my_actions = add_auxiliary_var + sub + add_constraint1 + add_constraint2
+
+# Build mod
+EliminateSingularityFractionMod = (my_match & my_filter).then(my_actions)
