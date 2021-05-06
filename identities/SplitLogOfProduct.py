20c9f80832cf15afb318ccf4b21106c78b600669
nikoskazazakis <nikos.kazazakis@octeract.co.uk>
2021-05-06 16:56:16 +0100

    fixed bug

--- identities/SplitLogOfProduct.py
+++ identities/SplitLogOfProduct.py
@@ -8,16 +8,9 @@ from octeract import *
 my_match = Match("C(n)*log(E(f)*E(g))")
 
 # Only select the ones that might cross into negative domains
-my_filter = IsMaybeFeasible("f*g >= 0") 
+my_filter = IsFullyFeasible("f >= 0") & IsFullyFeasible("g >= 0")
 
 # Split logarithm
-action1 = SubWith("n*log(f)+n*log(g)")
+action = SubWith("n*log(f)+n*log(g)")
 
-# Add constraints to enforce positivity
-action2 = AddConstraint("force_positive_f_log", "f>=0")
-action3 = AddConstraint("force_positive_g_log", "g>=0")
-
-# Chain actions
-my_actions = action1 + action2 + action3
-
-SplitLogOfProductMod = (my_match & my_filter).then(my_actions)
+SplitLogOfProductMod = (my_match & my_filter).then(action)
